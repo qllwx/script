6aec944cf51f9e261b1036e4a78cbeb0bef492d5
qllwx <qllwx@live.com>
2022-05-26 14:10:25 +0800

    Add files via upload
    update

--- xuexi.py
+++ xuexi.py
@@ -5,7 +5,17 @@ import sqlite3,json,os,re,random
 import pandas as pd
 from snownlp import SnowNLP
 
+URL={'专项答题':'https://pc.xuexi.cn/points/exam-paper-list.html',
+     '每日答题':'https://pc.xuexi.cn/points/exam-practice.html',
+     '我的积分':'https://pc.xuexi.cn/points/my-points.html',
+     '积分排行':'https://pc.xuexi.cn/points/points-rank.html',
+     '积分规则':'https://pc.xuexi.cn/points/points-rule.html',
+     '积分记录':'https://pc.xuexi.cn/points/points-record.html',
+     '每周答题':'https://pc.xuexi.cn/points/exam-weekly-list.html',
+     }
+
 global played
+global my_points
 con=sqlite3.connect("xuexi.db")
 cur=con.cursor()
 sql='create table if not exists dt (id integer primary key,tx text,body text,answers text,ts text,answer text)'
@@ -36,7 +46,9 @@ def login(page):
     page.mouse.wheel(0,1024)    
     while not page.title() == "我的积分":
         sleep(1)
-    print(page.title())        
+    print(page.title())
+    sleep(3)
+    put_my_point(page)        
     return page
 
 # Click text=每日答题
@@ -45,8 +57,9 @@ def go_exam_practice(page):
     sleep(2)
     expect(page).to_have_url("https://pc.xuexi.cn/points/exam-practice.html")
     print(page.title())
-    for count in range(200):
+    for count in range(20):
         exam_practice(page)
+        print(count)
 
 def get_line_feed(page,get_answer_list=False):
     answer =[]
@@ -98,6 +111,7 @@ def put_answer(page,answer):
     sleep(0.5)
     ans_i=0
     tx=page.locator("div.q-header").text_content().split()[0]
+    tx=tx[:3]
     body=page.locator("div.q-body").text_content()
     if tx=='多选题':
         answer_count=count_page_kh(page)
@@ -107,7 +121,11 @@ def put_answer(page,answer):
             print("全选")
             for i in range(page.locator("div.q-answers >> div ").count()):
                 page.locator("div.q-answers >> div").nth(i).click()
-            page.locator("button").click()
+            for i in range(page.locator("button").count()):
+                obj=page.locator("button").nth(i)
+                if obj.is_enabled():
+                    obj.click()
+            sleep(1)
             return
 
     if tx=='单选题':
@@ -126,15 +144,19 @@ def put_answer(page,answer):
                 continue
             ans_text=page.locator("div.q-answers >> div").nth(ans_i).inner_text()
             ans_zhongwen=re.sub('[^\u4E00-\u9FA5]',"", ans_text)
-            text_zhongwen=re.sub('[^a-z|A-Z|\d|\u4E00-\u9FA5]',"", text)
+            text_zhongwen=re.sub('[^a-z|A-Z|\d|\u4E00-\u9FA5|\.]',"", text)
             if  len(re.findall(text,ans_zhongwen))>0:
                 page.locator("div.q-answers >> div").nth(ans_i).click()
                 continue
             try:
                 page.locator("div.q-answers >> div:has-text('%s')"%text_zhongwen).nth(0).click()
             except:
-                #没有与提示相同的答案，采用关键词选择               
-                keywords=SnowNLP(text).keywords(limit=1)[0]
+                #没有与提示相同的答案，采用关键词选择 
+                keywords=SnowNLP(text).keywords(limit=1) 
+                if len(keywords)>0:             
+                    keywords=keywords[0]
+                else:
+                    keywords=text[:2]
                 k_count=page.locator("div.q-answers >> div:has-text('%s')"%keywords).count()
                 print("keywords:",keywords,k_count)
                 if k_count==0:
@@ -159,9 +181,11 @@ def put_answer(page,answer):
                
                 #page.locator("div.q-answer").nth( ans_i).click()
         ans_i+=1
-
-    print(page.locator("button").inner_text())
-    page.locator("button").click()  
+    if page.locator("button").count() == 1:
+        print(page.locator("button").first.inner_text())
+        page.locator("button").first.click()  
+    else:
+        page.locator("button:has-text('交 卷')").click()
             
 def is_video(page):
     pop_text=get_line_feed(page)
@@ -200,6 +224,7 @@ def get_exam(page):
     answer_count=0
     try:
         tx=page.locator("div.q-header").text_content().split()[0]
+        tx=tx[:3]
     except:
         if page.locator("button").inner_text() in ['再来一组','下一题','确定']:
             page.locator("button").click()
@@ -214,9 +239,11 @@ def get_exam(page):
     ts=get_line_feed(page)
     d={'tx':tx,'body':body,'answers':answers,'ts':ts} 
     answer=[]
-    for item in d['ts'].split('['):
-        if item.find(']')>0:
-            answer.append(item.split(']')[0])
+    if ts:
+        if '[' in ts:    
+            for item in d['ts'].split('['):
+                if item.find(']')>0:
+                    answer.append(item.split(']')[0])
     if answer_count>0:
         answer=answer[:answer_count]
     ans='|'.join(answer)
@@ -227,6 +254,8 @@ def get_exam(page):
 
 
 def exam_practice(page):
+    if page.locator("button").count()>1:
+        return
     if page.locator("button").inner_text() in ['再来一组','下一题','确定']:
         page.locator("button").click()
         sleep(1)
@@ -242,6 +271,7 @@ def exam_practice(page):
         print(answer)
         d['answer']=answer
         save_data(d)
+        print(d,'已经保存',sep='\n')
         return
     #answer=get_answer_from_page(page)
     #ans='|'.join(answer)
@@ -338,37 +368,46 @@ def xdwz(page):
     global played
     played=0
     # Go to https://pc.xuexi.cn/points/my-points.html
-    page.locator("text=去看看").nth(0).click()
+    page.goto("https://pc.xuexi.cn/points/my-points.html")    
+    obj=page.locator("text=去看看").nth(0)
+    if obj.is_enabled():
+        obj.click()
+    else:
+        print("没有可以点击的按钮")
+        return
     print(page.title())
     sleep(2)
-    menu_list=page.locator("div.menu-list >> a").count()
-    print(menu_list)
-    for i in range(1,menu_list): 
-        m=page.locator("div.menu-list >> a").nth(i).text_content()       
-        print(m)
-        if os.getenv('xx_learnning'):
-            learning=os.getenv('xx_learnning').split(';')
-        else:
-            learning=[]
-        if m in learning:
-            continue
-        else:
-            os.putenv('xx_learnning',m+';'.join(learning))
-        with page.expect_popup() as popup_info:
-            page.locator("text="+m).first.click()
-            sleep(2)
-        page_i=popup_info.value        
-        click_first_sapn_text(page_i)
-        page_i.close()      
-        sleep(2)
-    sleep(5)
-    for row in range(page.locator('span').count()):
-        page.locator('span').nth(row).click()
-        print(page.locator('span').nth(row).inner_text())
-        sleep(5)
-        page.go_back()
+    with page.expect_popup() as popup_info:
+        page.locator("span:has-text('更多头条')").nth(0).click()
+    page1=popup_info.value
+    sleep(2)   
+    text_count=page1.locator("div.grid-cell >>div.text-link-item-title").count()    
+    print('text',text_count)
+    for text in range(text_count):
+        obj=page1.locator("div.grid-cell >>div.text-link-item-title").nth(text).locator("span.text").first
+        text_title=obj.inner_text()
+        print(text_title)
+        with page1.expect_popup() as popup_info:
+            obj.click()
+        page2=popup_info.value
+        print(page2.title())
+        page2.locator("button.voice-lang-switch").nth(0).click()
+        play_audio(page2)
+        #sleep(60)
+        page2.close()
+    page1.close()
+    sleep(2)
+def play_audio(page):
+    current_time=page.locator("span.y-audio-current").inner_text()
+    total_time=page.locator("span.y-audio-total").inner_text()
+    while not current_time==total_time:
+        current_time=page.locator("span.y-audio-current").inner_text()
+        print(current_time,total_time,end='\r')
+        sleep(1)
+
 
 def xx_tv(page):
+    #视频学习
     global played
     played=0
     # Go to https://pc.xuexi.cn/points/my-points.html
@@ -376,35 +415,172 @@ def xx_tv(page):
     page.goto('https://www.xuexi.cn/')
     with page.expect_popup() as popup_info:
         page.locator("text=学习电视台").nth(0).click()
-        sleep(1)
+        sleep(2)
     page1=popup_info.value
-    print(page1.title())
+    print(page1.title(),'学习电视台')
     with page1.expect_popup() as popup_info:
-        page1.locator("span:has-text('学习慕课')").nth(0).click()
+        page1.locator("span:has-text('第一频道')").nth(0).click()
     page2=popup_info.value
-    print(page2.title(),page2.locator("div#page-main").inner_text(),sep='\n')
-    text_count=page2.locator("div#page-main >> div.textWrapper").count()
-    print('div.textWrapper',text_count)
+    sleep(2)
+    print(page2.title(),'第一频道')
+    page2.locator("span:has-text('列表')").nth(0).click()
+    sleep(3)
+    text_count=page2.locator("div.grid-cell >>div.text-link-item-title").count()    
+    print('span.text',text_count)
     for text in range(text_count):
-        text_title=page2.locator("div.textWrapper").nth(text).inner_text()
+        obj=page2.locator("div.grid-cell >>div.text-link-item-title").nth(text).locator("span.text").first
+        text_title=obj.inner_text()
         print(text_title)
         with page2.expect_popup() as popup_info:
-            page2.locator("text=%s"%text_title).first.click()
+            obj.click()
         page3=popup_info.value
         print(page3.title())
-        sleep(120)
+        sleep(60)
+        page3.close()
+    page2.close()
+    
+    #专项答题
+#https://pc.xuexi.cn/points/exam-paper-list.html
+def zx_dt(page):
+    global my_points
+    page.goto(URL['专项答题'])  
+    sleep(2) 
+    item_count=page.locator("div.item").count()
+    print(page.title(),item_count,'专项答题')
+    for item in range(item_count):
+        obj=page.locator("div.item").nth(item)
+        zt=obj.locator("div.right").inner_text()
+        print(zt)
+        #开始答题
+        if '已满分' in zt:
+            print(obj.locator('div.left').inner_text(),zt)
+            #obj.locator('div.right').click()
+            continue
+        else:
+            if not my_points['专项答题']=='去答题':
+                continue
 
+                
+            print(obj.locator('div.left').inner_text(),zt)
+            obj.locator('div.right').click()
+            for count in range(10):
+                sleep(1)
+                d=get_exam(page)
+                print(list(d.values()))
+                if is_video(page):
+                    answer=get_right_answer(page) 
+                    print(answer)
+                    d['answer']=answer
+                    save_data(d)
+                    print(d,'已经保存',sep='\n')
+                    return
+                answer=d['answer'].split('|')
+                put_answer(page,answer)
+                save_data(d)
+                #print(page.locator("button").first.inner_text())
+                print(page.locator("button").last.inner_text(),end='\r')
+                sleep(5)
+                #exam_practice(page)
+            page.goto(URL['专项答题']) 
+            my_points['专项答题']=='已完成'
+
+def  put_my_point(page):
+    global my_points
+    my_points={}
+    card_button=get_card_buttonbox(page)
+    card_title=get_card_text(page)
+    card_points=get_card_points(page)
+    for i in range(len(card_button)):
+        my_points[card_title[i]]=card_button[i]
+    print(card_title,card_points,card_button,sep='\n')
+
+def listing_music(page2):
+    page2.goto("https://pc.xuexi.cn/points/my-points.html")
+    # Click text=去看看 >> nth=1
+    page2.locator("text=去看看").nth(1).click()
+    # expect(page2).to_have_url("https://www.xuexi.cn/")
+    # Click text=听音乐
+    with page2.expect_popup() as popup_info:
+        page2.locator("text=听音乐").click()
+    page3 = popup_info.value
+    # Click text=播放整张专辑
+    #page3.locator("text=播放整张专辑").click()
+    sleep(2)
+    play_music(page3)
+    page3.close()
+
+def play_music(page):
+    obj=page.locator("li.album-list-item")
+    item_count=obj.count()
+    for item in range(item_count):
+        print(obj.nth(item).inner_text())
+        obj.nth(item).locator('span.item-play').click()
+        sleep(60)
+
+def  mz_dt(page):
+    page.goto("https://pc.xuexi.cn/points/my-points.html")
+    obj=  page.locator("div.buttonbox").nth(5)
+    buttonbox_text=obj.inner_text() 
+    if buttonbox_text=='去答题':
+        obj.click()
+    else:
+        print("没有可以点击的按钮")
+        return
+    sleep(3)
+    obj=   page.locator("div.week") 
+    button_count=obj.count()
+    print(page.title(),button_count)    
+    for i in range(button_count):
+        print(obj.nth(i).inner_text())
+        button_text=obj.nth(i).locator('button').inner_text()
+        if not button_text=='重新答题':
+            obj.nth(i).locator('button').click()
+            sleep(3)
+            for count in range(5):
+                sleep(1)
+                d=get_exam(page)
+                print(list(d.values()))
+                if is_video(page):
+                    answer=get_right_answer(page) 
+                    print(answer)
+                    d['answer']=answer
+                    save_data(d)
+                    print(d,'已经保存',sep='\n')
+                    return
+                answer=d['answer'].split('|')
+                put_answer(page,answer)
+                save_data(d)
+                #print(page.locator("button").first.inner_text())
+                print(page.locator("button").last.inner_text(),end='\r')
+                sleep(5)
+                #exam_practice(page)
+            sleep(2)
+            page.goto(URL['每周答题'])
+            
+            
 
 
 if __name__ == '__main__':
      page=login(page)
      sleep(1)
-     card_button=get_card_buttonbox(page)
-     card_title=get_card_text(page)
-     card_points=get_card_points(page)
-     print(card_title,card_points,card_button,sep='\n')
-     xx_tv(page)
+     #专项答题
+     zx_dt(page)
+     
+     put_my_point(page) 
+     #听音乐
+     listing_music(page)
+     #每日答题
      go_exam_practice(page)
-     page.goto("https://pc.xuexi.cn/points/my-points.html")
-     xdwz(page)
+     #视频学习
+     xx_tv(page)
+    
+       
+     
+     #每周答题
+     mz_dt(page)
+        
+     #选读文章
+     xdwz(page) 
+     
+     
      page.close()
